d862ca888a6a47eff16593660a8726758b18b558
Lrd134 <larryc3200@gmail.com>
2022-05-31 16:24:50 -0400

    fixed user table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -21,8 +21,7 @@ ActiveRecord::Schema.define(version: 2022_05_31_191131) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password_hash"
-    t.string "salt"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
