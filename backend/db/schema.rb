aeaf25eb501758bdf06d03f241cf31e2ea081f2f
Lrd134 <larryc3200@gmail.com>
2022-05-31 16:47:08 -0400

    updated messages to have target

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_31_191131) do
+ActiveRecord::Schema.define(version: 2022_05_31_204258) do
 
   create_table "messages", force: :cascade do |t|
     t.integer "user_id"
     t.text "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
   end
 
   create_table "users", force: :cascade do |t|
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -6,6 +6,3 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
-User.create(username: "asshole", password:"1234")
-User.create(username: "firstUser", password:"1322")
