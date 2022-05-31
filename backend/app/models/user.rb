782bf299f31df200cd6557b1e13099411d53c401
Lrd134 <larryc3200@gmail.com>
2022-05-31 15:09:45 -0400

    validates usernames for profanity

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :messages, dependent :nullify
+  validates_with UsernameValidator
   validates :username, presence: true, uniqueness: true, length: { min: 3, max: 20 }
 end
