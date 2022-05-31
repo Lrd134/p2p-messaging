e4ef4ac6386e3ead2e91b4daa7573ded6e5c5f3e
Lrd134 <larryc3200@gmail.com>
2022-05-31 16:53:09 -0400

    added seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -6,3 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+first = User.create(username: "asshole", password:"1322")
+penny = User.create(username: "PennyPanda", password:"pandasROCK3!")
+hannah = User.create(username: "HappyHannah", password: "pandas3312$")
+peter = User.create(username: "PeterPiccolo", password: "!piccALot389@")
+
+Message.create(text: "Hello! How are you today?", user: hannah, username: penny)
+Message.create(text: "I'm well, and you?", user: penny, username: hannah)
+Message.create(text: "What's happs", user: peter, username: penny)
+Message.create(text: "Nothing much, what's up with you?", user: penny, username: peter)
+Message.create(text: "I just saw a paper on Science Fiction!", user: penny, username: hannah)
+Message.create(text: "That's so cool! I love Sci-Fi!", user: hannah, username: penny)
+Message.create(text: "I'm doing great! I just finished my essay on Floral Ambiguities", user: hannah, username: penny)
+Message.create(text: "I just saw your name on the user board! It's nice to meet you!", user: hannah, username: peter)
