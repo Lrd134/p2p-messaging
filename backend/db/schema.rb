0147fd3b45b814c9b7b4c6500809c3701d19ba20
Lrd134 <larryc3200@gmail.com>
2022-05-31 15:25:17 -0400

    migrated db

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_31_190955) do
+ActiveRecord::Schema.define(version: 2022_05_31_191131) do
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "user_id"
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
