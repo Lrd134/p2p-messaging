e13b7d038b769b27ba51ec368d5cb5861ae780cc
Lrd134 <larryc3200@gmail.com>
2022-06-02 15:07:44 -0400

    updated message serializer

--- backend/app/serializers/message_serializer.rb
+++ backend/app/serializers/message_serializer.rb
@@ -1,4 +1,16 @@
 class MessageSerializer
   include FastJsonapi::ObjectSerializer
-  set_key_transform :camel_lower
+  attribute :text
+  attribute :creator do | object |
+    {
+      :givenName => object.creator.username,
+      :sentAt => object.creator.created_at
+    }
+  end
+  attribute :target do | object |
+    {
+      :givenName => object.target.username,
+      :sentAt => object.target.created_at
+    }
+  end
 end
