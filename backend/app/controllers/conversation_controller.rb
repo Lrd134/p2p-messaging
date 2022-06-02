941e9a13be76910740e4a4fa5c86563dc2bd21dd
Lrd134 <larryc3200@gmail.com>
2022-06-02 12:39:46 -0400

    added conversation model and controller

---
+++ backend/app/controllers/conversation_controller.rb
@@ -0,0 +1,42 @@
+class ConversationsController < ApplicationController
+  before_action :set_conversation, only: %i[show delete update]
+  
+    def index
+      @conversation = Conversation.all
+      options = { is_collection: true }
+      render json: serialize_conversation(options)
+    end
+  
+    def show
+  
+    end
+  
+    def create
+      @conversation = Conversation.create conversation_params
+      render json: serialize_conversation
+    end
+  
+    def update
+      
+    end
+  
+  
+    def delete
+  
+    end
+  
+    private
+    
+    def conversation_params
+      params.require(:conversation).permit(:name, :description, :message)
+    end
+  
+    def serialize_conversation options = nil
+      ConversationSerializer.new(@conversation, options).serialized_json
+    end
+  
+    def set_conversation
+      @conversation = Conversation.find_by(conversationname: params.require(:conversation).permit(:conversationname))
+    end
+  
+  end
