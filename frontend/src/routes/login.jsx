e352a37c71269da9c710560291a093aef624e64b
Lrd134 <larryc3200@gmail.com>
2022-06-06 12:14:16 -0400

    updated login to a class component

--- frontend/src/routes/login.jsx
+++ frontend/src/routes/login.jsx
@@ -1,7 +1,32 @@
-export default function Login() {
-  return (
+import React, { Component } from 'react';
+import newUser from '../users/userDispatch'
+import { connect } from 'react-redux';
+class Login extends Component {
+  handleSubmit = event => {
+    event.preventDefault();
+
+  }
+  handleChange = event => {
+
+  }
+  render() {
+    return (
     <main>
-      <h2>Login</h2>
+      <h1>Login</h1>
+      <form onSubmit={this.handleSubmit}>
+        <input onChange={this.handleChange} value={this.props.username} name="username" type="text" placeholder="User Name"/>
+        <input onChange={this.handleChange} value={this.props.password} name="password" type="text" placeholder="Password"/>
+        <input onClick={this.handleSubmit} type="Submit" readOnly={true} value="Signup" onSubmit={this.handleSubmit}/>
+      </form>
     </main>
-  );
+    )
+  }
 }
+
+const mapDispatchToProps = dispatch => {
+  return {
+    newUser: (user) => dispatch(newUser(user))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Login);
