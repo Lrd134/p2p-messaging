3a33246ec27ca0e20fccaa23075cf807eb1bcd14
Lrd134 <larryc3200@gmail.com>
2022-06-06 08:32:16 -0400

    added users to reducer

--- frontend/src/reducers/rootReducer.js
+++ frontend/src/reducers/rootReducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux'
 
 import conversationReducer from './conversationReducer'
 import messageReducer from './messageReducer'
+import userReducer from './userReducer'
 
 const rootReducer = combineReducers({
   conversations: conversationReducer,
-  messages: messageReducer
+  messages: messageReducer,
+  users: userReducer
 })
 
 export default rootReducer
