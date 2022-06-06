644fa018aca292e627a75c746bdea8c2a95d1d71
Lrd134 <larryc3200@gmail.com>
2022-06-06 12:13:10 -0400

    created a dispatch method for a new user

---
+++ frontend/src/users/userDispatch.js
@@ -0,0 +1,33 @@
+import initRequest from '../actions/initialAction'
+import { loginAction } from '../actions/sessionActions'
+import badRequest from '../actions/badRequestAction'
+const newUser = (user) => {
+  return (dispatch) => {
+    dispatch(initRequest)
+    
+    fetch(process.env.REACT_APP_BACKEND_URL + '/signup', {
+      headers: {            
+      "Content-Type": "application/json",
+      Accept: "application/json"
+      },
+      method: "POST",
+      body: JSON.stringify({
+              user: {
+                username: user.username,
+                password: user.password
+              }})
+
+      }).then(resp => resp.json()).then(json => {
+        console.log("received \n\n")
+        console.log(json)
+        if (typeof json === Array) {
+          alert(json[0][0])
+          dispatch(badRequest)
+          return;
+        }
+        dispatch(loginAction(json))
+      }).catch(error => console.log(error))
+  }
+}
+
+export default newUser;
