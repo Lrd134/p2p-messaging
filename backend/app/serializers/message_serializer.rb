138d4daa5a2076a2e6c4860ad0c7ec5901dda13b
Lrd134 <larryc3200@gmail.com>
2022-06-08 16:52:27 -0400

    added userId to message serializer

--- backend/app/serializers/message_serializer.rb
+++ backend/app/serializers/message_serializer.rb
@@ -4,12 +4,14 @@ class MessageSerializer
   attribute :creator do | object |
     {
       :givenName => object.creator.username,
+      :userId => object.creator.id,
       :sentAt => object.creator.created_at
     }
   end
   attribute :target do | object |
     {
       :givenName => object.target.username,
+      :userId => object.creator.id,
       :sentAt => object.target.created_at
     }
   end
