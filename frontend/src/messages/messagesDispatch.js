bc93843e75f9bb80d37734e8da0ff4587b5f8815
Lrd134 <larryc3200@gmail.com>
2022-06-09 15:05:41 -0400

    created messageDispatch

---
+++ frontend/src/messages/messagesDispatch.js
@@ -0,0 +1,30 @@
+import { getMessagesAction, messageRequestAction } from '../actions/messageActions'
+const getMessages = user => {
+  return dispatch => {
+    dispatch(messageRequestAction)
+    fetch(process.env.REACT_APP_BACKEND_URL + '/messages', {
+      headers: {            
+      "Content-Type": "application/json",
+      Accept: "application/json"
+      },
+      method: "POST",
+      body: JSON.stringify({
+              user: {
+                username: user.username,
+                password: user.password
+              }})
+
+      }).then(resp => resp.json()).then(json => {
+        console.log("received \n\n")
+        console.log(json)
+        if (typeof json === Array) {
+          alert(json[0][0])
+          
+          return;
+        }
+        dispatch(getMessagesAction(json))
+      }).catch(error => console.log(error))
+  }
+}
+
+export { getMessages };
