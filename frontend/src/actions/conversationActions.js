b9c56c5cbfea69a5f2fe61397eab4900eaa56262
Lrd134 <larryc3200@gmail.com>
2022-06-15 15:12:31 -0400

    reverses data to show latest message

--- frontend/src/actions/conversationActions.js
+++ frontend/src/actions/conversationActions.js
@@ -2,12 +2,15 @@ const getConversationsAction = data => {
   const found = [];
   const latest = [];
   const getLatestConversations = data => {
+    
     if (!found.includes(data.attributes.conversation_id)) {
       latest.push(data)
       found.push(data.attributes.conversation_id)
     }
   }
+  data.data.reverse()
   data.data.forEach(getLatestConversations);
+  data.data.reverse()
   return {
     payload: latest,
     type: "GET_CONVERSATIONS"    
