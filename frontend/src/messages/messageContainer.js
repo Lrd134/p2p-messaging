cb666867a8475d873c51e91a4ff943753b350c8d
Lrd134 <larryc3200@gmail.com>
2022-06-15 15:11:00 -0400

    added new message form

---
+++ frontend/src/messages/messageContainer.js
@@ -0,0 +1,31 @@
+import { useParams } from 'react-router-dom';
+import { useState } from 'react';
+import Message from './message.js';
+
+function MessageContainer(props) {
+  const { conversationId } = useParams();
+  const [message, setMessage] = useState("");
+
+  const renderMessages = () => props.messages.messages.filter(message => parseInt(message.attributes.conversation_id) === parseInt(conversationId))
+   .map(message => <Message key={message.id} message={message} />)
+
+  const handleSubmit = event => {
+    event.preventDefault()
+  }
+
+  const handleChange = event => {
+    setMessage(event.target.value);
+  }
+  return (
+    <main>  
+      {renderMessages()}
+      <form onSubmit={handleSubmit}>
+      <input type="text" value={message} name="message" onChange={handleChange} />
+
+      </form>
+    </main>
+  )
+}
+
+
+export default MessageContainer;
